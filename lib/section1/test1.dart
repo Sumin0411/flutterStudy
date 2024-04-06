b5abc14dd005fd169970eeff7b799f64d365379b
Sumin0411 <kimsumin411@gmail.com>
2024-04-06 20:36:20 +0900

    문자열 템플릿 : const

--- lib/section1/test1.dart
+++ lib/section1/test1.dart
@@ -1,9 +1,18 @@
-main(){
-  int no = 10;
-  String name = 'kkang';
+// main(){
+//   int n1 = 10;
+//   double d1 = 10.0;
+//
+//   double d2 = n1;
+//   int n2 = d1;
+// }
+const String data1 = 'hello';
 
-  String myFun(){
-    return 'kim';
+class User{
+  static const String data2 = 'hello';
+
+  void some(){
+    const String data3 = 'hello';
+
+    // data1 = 'world';
   }
-  print('no: $no, name: $name, myFun() : ${myFun()}');
 }
