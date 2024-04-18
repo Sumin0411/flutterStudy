3cb63751d857c7f17b2b502ab8cd1cd66b3dfffc
Sumin0411 <kimsumin411@gmail.com>
2024-04-18 23:11:10 +0900

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,44 +4,108 @@ void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  bool enabled = false;
-  String stateText = "disable";
-  void chageCheck(){
-    if (enabled) {
-      stateText = "disable";
-      enabled = false;
-    } else {
-      stateText = "enable";
-      enabled = true;
-    }
-  }
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Stateless Test'),
+          title: const Text('Stateful Test'),
         ),
-        body: Center(
-            child:Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: (enabled ?  const Icon(Icons.check_box, size : 20,):
-                        const Icon(Icons.check_box_outline_blank, size: 20,)),
-                  color: Colors.red,
-                  onPressed: chageCheck,
-                ),
-                Container(
-                  padding: const EdgeInsets.only(left:16),
-                  child: Text('$stateText', style: const TextStyle(fontSize: 32,
-                  fontWeight: FontWeight.bold),),
-                ),
-              ],
-            ),)
+        body: MyWidget(),
       ),
     );
   }
 }
+class MyWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return _MyWidgetState();
+  }
+}
+
+class _MyWidgetState extends State<MyWidget>{
+  bool enabled = false;
+  String stateText = "disable";
+
+  void changeCheck(){
+    setState(() {
+      if (enabled){
+        stateText = "disable";
+        enabled = false;
+      }else{
+        stateText = "enable";
+        enabled = true;
+      }
+    });
+  }
+
+  @override
+    Widget build(BuildContext context){
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            icon: (enabled
+                ? const Icon(
+              Icons.check_box,
+              size: 20,
+          )
+        :  const Icon(Icons.check_box_outline_blank, size: 20,)),
+          color: Colors.red,
+          onPressed: changeCheck,
+          ),
+          Container(
+            padding: const EdgeInsets.only(left:16),
+            child: Text(
+              '$stateText',
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  }
+// class MyApp extends StatelessWidget {
+//   bool enabled = false;
+//   String stateText = "disable";
+//   void chageCheck(){
+//     if (enabled) {
+//       stateText = "disable";
+//       enabled = false;
+//     } else {
+//       stateText = "enable";
+//       enabled = true;
+//     }
+//   }
+//   @override
+//   Widget build(BuildContext context){
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Stateless Test'),
+//         ),
+//         body: Center(
+//             child:Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 IconButton(
+//                   icon: (enabled ?  const Icon(Icons.check_box, size : 20,):
+//                         const Icon(Icons.check_box_outline_blank, size: 20,)),
+//                   color: Colors.red,
+//                   onPressed: chageCheck,
+//                 ),
+//                 Container(
+//                   padding: const EdgeInsets.only(left:16),
+//                   child: Text('$stateText', style: const TextStyle(fontSize: 32,
+//                   fontWeight: FontWeight.bold),),
+//                 ),
+//               ],
+//             ),)
+//       ),
+//     );
+//   }
+// }
