b9e864b138c0c6d66fd01079deba35972ba9677f
Sumin0411 <kimsumin411@gmail.com>
2024-04-18 22:10:26 +0900

    StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,43 @@ void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
+  bool enabled = false;
+  String stateText = "disable";
+  void chageCheck(){
+    if (enabled) {
+      stateText = "disable";
+      enabled = false;
+    } else {
+      stateText = "enable";
+      enabled = true;
+    }
+  }
+
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Test'),
+          title: const Text('Stateless Test'),
         ),
-        body: Center(child: GestureDetector(child: const Text('HelloWorld'))),
+        body: Center(
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: (enabled ?  const Icon(Icons.check_box, size : 20,):
+                        const Icon(Icons.check_box_outline_blank, size: 20,)),
+                  color: Colors.red,
+                  onPressed: chageCheck,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left:16),
+                  child: Text('$stateText', style: const TextStyle(fontSize: 32,
+                  fontWeight: FontWeight.bold),),
+                ),
+              ],
+            ),)
       ),
     );
   }
